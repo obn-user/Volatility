5be872060897262b5fe25db995691f49e058e4dc
dkoch <dkoch@DESKTOP-D9R7QIL>
2026-09-07 18:21:48 +0300

    66

--- ViewModel/VM.cs
+++ ViewModel/VM.cs
@@ -5,7 +5,7 @@ using Test.Model.DataFormat;
 using Test.Model.Tester.Spread_Volatility;
 
 namespace Test.ViewModel
-{//7879989__11_22_33_44_55
+{//7879989__11_22_33_44_55_66
     public class VM : BaseVM
     {
 
