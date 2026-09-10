86b1e07fd7f09c390cce19f556466c210a4ae5c3
dkoch <dkoch@DESKTOP-D9R7QIL>
2026-09-10 23:07:50 +0300

    конструктор класса парам, создающий словарь строчных параметров

--- Model/Param/Param.cs
+++ Model/Param/Param.cs
@@ -9,6 +9,22 @@ namespace Test.Model.Param
 {
     public class Param
     {
+     public   Dictionary<string, string> Params = [];
+        public Param()
+        {
+            var people = new Dictionary<string, string>()
+{
+    { "3", "Tom"},
+    { "3", "Sam"},
+    { "3", "Bob"}
+};
+        }
+
+
+
+        /*
+
+
         public string FileName = "Si626926_Price.txt";
 
         public decimal InitialDeposit { get; set; } = 1000000;
@@ -24,7 +40,9 @@ namespace Test.Model.Param
         static decimal fixedPartl = 0;
         static decimal percentPart = 1 / 10000;
         public Exchange_Comission Exchange_Comission = new(fixedPartl, percentPart);
+        */
     }
+
 }
     
 
